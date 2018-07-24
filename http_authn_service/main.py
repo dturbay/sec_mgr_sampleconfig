a7886e32876bdf1db7f9bd892a6e33a8a57bf1ac
Denys Turbai <dturbai@dturbai.svl.corp.google.com>
2018-07-24 11:36:13 -0700

    Update submodules

--- http_authn_service/main.py
+++ http_authn_service/main.py
@@ -8,8 +8,29 @@ def check(user, pw):
 
 @route('/auth')
 @auth_basic(check)
-def hello():
+def auth():
   return "Hello World!"
+
+
+# resources
+
+def parametrized_check(predefined_user, predefined_password):
+  def check(user, password):
+    return predefined_user == user and predefined_password == password
+  return check
+
+
+@route('/resource1')
+@auth_basic(parametrized_check('user1', 'password1'))
+def resource1():
+  return "Hello resource1!"
+
+@route('/resource2')
+@auth_basic(parametrized_check('user2', 'password2'))
+def resource2():
+  return "Hello resource2!"
+
+
 
 run(host='0.0.0.0', port=4444, debug=True)
 
